787d6e105c5ea0e1ea44c02c34804a340ee98d47
ishushan02 <ishananand2002@gmail.com>
2024-10-23 16:58:43 -0700

    Read the File and extract the Data

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Transformer-Implementation
+
+- Download the Book(https://mankshop.in/wp-content/uploads/2024/08/OceanofPDF.com_I_dont_love_you_anymore_-_Rithvik_singh_rathore.pdf?srsltid=AfmBOooIQvJAM25vfy3UlLL3ceWAKcq6HD7_BfStcBQ5_Niy9CXutvVE) an execute get_data.py to extract the texts from the PDF.
+
+- The extracted text will be present in extracted_text.txt
+
---
+++ get_data.py
@@ -0,0 +1,34 @@
+import PyPDF2
+
+
+import pdfplumber
+
+def extract_text_from_pdf(pdf_path):
+    with pdfplumber.open(pdf_path) as pdf:
+        text = ''
+        for page in pdf.pages:
+            text += page.extract_text() + '\n'
+    
+    print(text[:1000])
+    return text
+
+
+# def extract_text_from_pdf(pdf_path):
+#     # Open the PDF file
+#     with open(pdf_path, 'rb') as file:
+#         reader = PyPDF2.PdfReader(file)
+#         text = ''
+#         # Iterate through each page and extract text
+#         for page in reader.pages:
+#             text += page.extract_text() + '\n'
+#     return text
+
+# Example usage
+pdf_file_path = '/Users/ishananand/Desktop/Transformers/Rithivik_Singh.pdf'  # Replace with your PDF file path
+extracted_text = extract_text_from_pdf(pdf_file_path)
+
+# Save the extracted text to a .txt file
+with open('extracted_text.txt', 'w', encoding='utf-8') as text_file:
+    text_file.write(extracted_text)
+
+print("Text extraction complete! Check 'extracted_text.txt' for results.")
